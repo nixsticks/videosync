ca9611ab6a7e8d933398bc070dc0f807ff10c72e
Nikki Thean <nikki.thean@gmail.com>
2014-01-05 14:09:46 -0500

    Change redistogourl

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ Bundler.require
 module VideoSync
   class App < Sinatra::Application
     configure do
-      uri = URI.parse(ENV["redis://redistogo:be02e5dde25766891f89beaec935aeb3@albacore.redistogo.com:9968/"])
+      uri = URI.parse(ENV["REDISTOGO_URL"])
       REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
     end
     
