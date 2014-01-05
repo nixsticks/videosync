3bbd3335c7f65ae9f835c96a5d5bf537af0536d8
Nikki Thean <nikki.thean@gmail.com>
2014-01-05 14:05:47 -0500

    Add redistogo

--- app.rb
+++ app.rb
@@ -3,6 +3,11 @@ Bundler.require
 
 module VideoSync
   class App < Sinatra::Application
+    configure do
+      uri = URI.parse(ENV["redis://redistogo:be02e5dde25766891f89beaec935aeb3@albacore.redistogo.com:9968/"])
+      REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+    end
+    
     get '/' do
       haml :index
     end
