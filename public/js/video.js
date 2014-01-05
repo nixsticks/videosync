6e69958240c2a6e990532b9fef1d7eded9bc6f40
Nikki Thean <nikki.thean@gmail.com>
2014-01-05 18:49:07 -0500

    Chatroom experiment

--- public/js/video.js
+++ public/js/video.js
@@ -44,6 +44,14 @@ function onytplayerStateChange(newState) {
   }
 }
 
+$("#input-form").on("submit", function(event) {
+  event.preventDefault();
+  var handle = $("#input-handle")[0].value;
+  var text   = $("#input-text")[0].value;
+  ws.send(JSON.stringify({ handle: handle, text: text }));
+  $("#input-text")[0].value = "";
+});
+
 ws.onmessage = function(message) {
   var data = JSON.parse(message.data);
   if (data == "PLAYING") {
@@ -51,6 +59,12 @@ ws.onmessage = function(message) {
   }
   else if (data == "ENDED" || data == "PAUSED" || data == "BUFFERING") {
     ytplayer.pauseVideo();
+  }
+  else if (typeof data == ("Object")) {
+    $("#chat-text").append("<div class='panel panel-default'><div class='panel-heading'>" + data.handle + "</div><div class='panel-body'>" + data.text + "</div></div>");
+    $("#chat-text").stop().animate({
+      scrollTop: $('#chat-text')[0].scrollHeight
+    }, 800);
   }
   else {
     time = ytplayer.getCurrentTime();
